1fc2bba92acb487ecf0f14981047ab8bcbc99b62
tinh2044 <tinhnguyen23122004@gmail.com>
2022-09-19 22:07:56 +0700

    up

---
+++ src/routes/index.js
@@ -0,0 +1,32 @@
+import HomePage from '~/pages/Home';
+import Following from '~/pages/Following';
+import Profile from '~/pages/Profile';
+import Upload from '~/pages/Upload';
+import Search from '~/pages/Search';
+import { HeaderOnly } from '~/components/Layout';
+const publicRoutes = [
+    {
+        path: '/',
+        component: HomePage,
+    },
+    {
+        path: '/following',
+        component: Following,
+    },
+    {
+        path: '/profile',
+        component: Profile,
+    },
+    {
+        path: '/upload',
+        component: Upload,
+        layout: HeaderOnly,
+    },
+    {
+        path: '/search',
+        component: Search,
+        layout: null,
+    },
+];
+const privateRoute = [];
+export { publicRoutes, privateRoute };
